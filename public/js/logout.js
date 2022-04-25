cd3fa8bd3ece5a1e26439c6cdffbd4ffd3e28f0b
Matthew Carson <matthew.carson@pm.me>
2022-04-25 02:26:24 -0400

    Clean up FE logout function

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,5 @@
 document.querySelectorAll('.logout_form').forEach(el => {
   el.addEventListener('submit', async e => {
-    console.log('here')
     e.preventDefault()
     await fetch('/logout', {
       method: 'DELETE'
