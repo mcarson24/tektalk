f30e1f4bfba7da54b1d2440f8bbd7eb2aa7ab73c
Matthew Carson <matthew.carson@pm.me>
2022-04-26 21:40:18 -0400

    Messing around with Heroku

--- config/db.js
+++ config/db.js
@@ -3,15 +3,22 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export default new Sequelize(process.env.JAWSDB_URL ||
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
-  }
-)
+let db
+
+if (process.env.JAWSDB_URL) {
+  db = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  db = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USERNAME, 
+    process.env.DB_PASSWORD, 
+    {
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  )
+}
+export default new Sequelize(db)
 
 
