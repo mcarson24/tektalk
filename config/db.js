d61927a3763b311bffe994c3af688f44649cea80
Matthew Carson <matthew.carson@pm.me>
2022-04-26 21:22:50 -0400

    Correct the JAWSDB_URL variable

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export default new Sequelize(process.env.JAWS_DB ||
+export default new Sequelize(process.env.JAWSDB_URL ||
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
